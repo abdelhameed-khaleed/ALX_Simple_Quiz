03cf49e4ea7b11a0b8aa8af40ff554f2ddea603a
Abdelhameed <am97130811@yahoo.com>
2024-07-19 20:48:54 +0300

    add correctAnswer variable

--- quiz.js
+++ quiz.js
@@ -1,10 +1,10 @@
 function checkAnswer() {
 
-    const checkAnswer =4;
+    const correctAnswer =4;
 
     let userAnswer = document.querySelector("input[name='quiz']:checked").value 
     // The above line is what I learnerd in this task
-    if(userAnswer == checkAnswer){
+    if(userAnswer == correctAnswer){
         document.getElementById("feedback").textContent = "Correct! Well done.";
     }
     else{
