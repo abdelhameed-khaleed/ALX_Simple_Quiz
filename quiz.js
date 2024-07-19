bb747d46f82217e3a775e43bc64379a280b71f19
Abdelhameed <am97130811@yahoo.com>
2024-07-19 17:14:13 +0300

    add task

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer() {
+
+    const checkAnswer =4;
+
+    let userAnswer = document.querySelector("input[name='quiz']:checked").value 
+    // The above line is what I learnerd in this task
+    if(userAnswer == checkAnswer){
+        document.getElementById("feedback").textContent = "Correct! Well done.";
+    }
+    else{
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!"
+    }
+
+}
+
+
+document.getElementById("submit-answer").addEventListener('click',checkAnswer);
